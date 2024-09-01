75c78e9e3d29513ba4b192c9c61e70f2fd93c50d
mohabbaticicle <mohabbatali@iciclecorporation.com>
2024-09-01 10:18:07 +0600

    some change

--- src/components/Common/ProductCard/ProductCard.jsx
+++ src/components/Common/ProductCard/ProductCard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./ProductCard.css";
 import { FaRegHeart, FaShoppingCart, FaStar } from "react-icons/fa";
 import { GiRecycle } from "react-icons/gi";
+import { MdOutlineShoppingBag } from "react-icons/md";
 
 const ProductCard = ({ item }) => {
+  console.log(item);
   return (
     <div className="card border border-indigo-600 border-opacity-20">
-      <div className="w-full h-[300px] p-3">
-        <img className="w-full h-full" src={item.img} alt="" />
+      <div className="w-full h-[300px] ">
+        <img className="w-full h-full p-5" src={item.img} alt="" />
       </div>
       <div className=" h-[150px] px-4 py-5">
         <div className="flex gap-1">
@@ -17,10 +19,8 @@ const ProductCard = ({ item }) => {
           <FaStar />
         </div>
 
-        <p className="card-info">
-         {item?.name}
-        </p>
-        <div className="flex gap-2 my-2">
+        <p className="card-info my-2">{item?.title}</p>
+        <div className="flex gap-2 my-4 ml-[50px] ">
           <del className="text-xl">$99.99</del>
           <p>$80.99</p>
         </div>
@@ -31,7 +31,10 @@ const ProductCard = ({ item }) => {
         <FaRegHeart size={30} />
         {/* <FaShoppingCart className="text-2xl" /> */}
       </div>
-      <button className="card-btn text-white bg-indigo-700">Add to Cart</button>
+      <button className="card-btn  bg-orange-700 mx-auto justify-center flex whitespace-nowrap items-center gap-2">
+        <MdOutlineShoppingBag className="text-xl" />
+        <span className="mt-[2px] font-semibold">Add to Cart</span>
+      </button>
     </div>
   );
 };
